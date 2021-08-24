09102338e84301bd0365f89fcaed0ed64ab1c9cc
andreaslebherz <lebherz@student.tugraz.at>
2021-08-24 11:00:40 +0200

    Added main script and helper functions to load scenario, simulate and export detections

---
+++ helperExtendedTargetDistance.m
@@ -0,0 +1,22 @@
+function dist = helperExtendedTargetDistance(track,truth)
+% This function computes the distance between track and a truth.
+
+% Copyright 2019-2020 The MathWorks, Inc.
+
+% Errors in each aspect
+[posError,velError,dimError,yawError] =  helperExtendedTargetError(track,truth);
+
+% For multiObjectTracker, add a constant penalty for not estimating yaw
+% and dimensions
+if isnan(dimError)
+    dimError = 1;
+end
+if isnan(yawError)
+    yawError = 1;
+end
+
+% Distance is the sum of errors
+dist = posError + velError + dimError + yawError;
+
+
+end
---
+++ mainDemoGhosting.m
@@ -0,0 +1,81 @@
+clc;
+clear;
+close all;
+
+%% Define constants
+n_radar = 5;
+id_ego = 1;
+
+% export .csv file in folder output with detections
+EXPORT_CSV = true;
+
+%% Load scenario and init plot window
+
+% Create the scenario
+addpath('scenarios');
+
+% handle for exported driving scenario
+% scenarioHandle = @demo_ghost_detections;
+scenarioHandle = @demo_ghost_detections_2;
+% scenarioHandle = @demo_ghost_highway_ramp;
+filename = 'demo_ghost_detections_2';
+
+% create scenario-obj, ego-vehicle motion and sensor setup
+[scenario, egoVehicle, sensors] = helperCreateScenario(scenarioHandle);
+
+numRadar = sum(cellfun(@(s) isa(s, 'radarDataGenerator'), sensors, 'UniformOutput', true));
+% sanity check
+assert(id_ego==egoVehicle.ActorID, sprintf('Ego vehicle needs ID: %d.', id_ego));
+assert(n_radar==numRadar, sprintf('Setup needs a number of sensors: %d.', n_radar));
+
+% Create the display object
+display = helperExtendedTargetTrackingDisplay;
+% Set this to vehicle ID you want to follow
+display.FollowActorID = 2;
+
+% Create the Animation writer to record each frame of the figure for
+% animation writing. Set 'RecordGIF' to true to enable GIF writing.
+gifWriter = helperGIFWriter('Figure',display.Figure,...
+    'RecordGIF',true);
+
+%% Running scenario
+% Reset the random number generator for repeatable results
+seed = 2021;
+S = rng(seed);
+
+% Tranformation is necessary for correct plotting
+for i = 1:numRadar
+        sensors{i}.HasRangeRate = false;
+        sensors{i}.DetectionCoordinates = 'Body';
+        sensors{i}.TargetReportFormat = 'Detections';
+        sensors{i}.HasElevation = false;
+end
+
+timeStep = 1;
+
+% Run the scenario
+while advance(scenario) && ishghandle(display.Figure)
+    % Get the scenario time
+    time = scenario.SimulationTime;
+
+    % Collect detections from the ego vehicle sensors
+    [detections,isValidTime] = helperDetect(sensors, egoVehicle, time);
+   
+    % Update the tracker if there are new detections
+    if any(isValidTime)
+        % Display detections
+        display(egoVehicle, sensors, detections);
+        drawnow;
+        if EXPORT_CSV
+            export_sensor_data(scenario, detections, egoVehicle, strcat('output/', filename));
+        end
+        % Update timeStep
+        timeStep = timeStep + 1;
+
+        % Capture frames for animation
+        gifWriter();
+    end
+end
+
+% Write GIF
+writeAnimation(gifWriter, strcat('output/', filename));
